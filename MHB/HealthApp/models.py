e07b5297a16512c93593150f0e4ae2b353a6d029
Ved K <vedkorla300@gmail.com>
2023-03-31 16:37:20 +0530

    Created a model for Users and Therapists

--- MHB/HealthApp/models.py
+++ MHB/HealthApp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class User(models.Model):
+    user = models.OneToOneField(User, 
+    on_delete=models.CASCADE,
+    primary_key=True)
+    concerns = models.CharField(max_length=256)
+
+
+class Therapists(models.Model):
+    full_name = models.CharField(null = False,blank = False,max_length=80)
+    phone = models.IntegerField(null = False,blank = False,unique = True)
+    email = models.EmailField(unique=True, max_length=254)
+    yoe = models.IntegerField(blank=False)
